185cd9c817c8a28a0667d697ee39b0c3280226df
Enam <mdenamahmedchowdhury@gmail.com>
2024-02-28 18:58:39 +0600

    work in front end pages section

--- src/pages/frontend/page-section/PageSection.jsx
+++ src/pages/frontend/page-section/PageSection.jsx
@@ -2,6 +2,9 @@ import { Box, Grid, OutlinedInput, Stack, Tab } from "@mui/material";
 import React from "react";
 import PagenameContainer from "../../../layout/pagename-container/PagenameContainer";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { ClipboardType, Contact, Home, ShieldQuestion } from "lucide-react";
 
 const PageSection = () => {
   const [value, setValue] = React.useState("1");
@@ -10,34 +13,77 @@ const PageSection = () => {
     setValue(newValue);
   };
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <Box className="page-container">
       <PagenameContainer>Website Page</PagenameContainer>
       <Box className="page-main-content">
-        <Grid container>
-          <Grid item sm={12} md={4} lg={2} className="card1" mr={2}>
-            <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <TabContext value={value}>
+          <Grid container spacing={2}>
+            <Grid item sm={12} md={4} lg={2}>
+              <Box className="card1">
                 <TabList
                   onChange={handleTabChange}
                   aria-label="lab API tabs example"
+                  orientation="vertical"
+                  sx={{
+                    ".MuiTab-root": {
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "start",
+                    },
+                    ".MuiTab-iconWrapper": {
+                      marginRight: 1, // Add a margin between the icon and the label
+                    },
+                  }}
                 >
-                  <Tab label="Slider List" value="1" />
-                  <Tab label="Add Slider" value="2" />
+                  <Tab label="Home" value="1" icon={<Home />} />
+                  <Tab label="Appoinment" value="2" icon={<ClipboardType />} />
+                  <Tab label="Doctors" value="3" icon={<ClipboardType />} />
+                  <Tab label="About us" value="4" icon={<Home />} />
+                  <Tab label="FAQ" value="5" icon={<ShieldQuestion />} />
+                  <Tab label="Contact us" value="6" icon={<Contact />} />
                 </TabList>
               </Box>
-              <TabPanel value="1">
-                <Stack direction="row" justifyContent="end">
-                  <OutlinedInput size="small" placeholder="Search patient" />
-                </Stack>
+            </Grid>
 
-                {/* <SliderList /> */}
-              </TabPanel>
-              <TabPanel value="2">{/* <AddSlider /> */}</TabPanel>
-            </TabContext>
+            <Grid item sm={12} md={8} lg={10}>
+              <Box className="card1">
+                <TabPanel value="1">
+                  <Stack direction="row" justifyContent="end">
+                    <OutlinedInput size="small" placeholder="Search patient" />
+                  </Stack>
+                  Panel 1
+                </TabPanel>
+                <TabPanel value="2">Panel 2</TabPanel>
+                <TabPanel value="3">Panel 3</TabPanel>
+                <TabPanel value="4">Panel 4</TabPanel>
+                <TabPanel value="5">Panel 5</TabPanel>
+                <TabPanel value="6">Panel 6</TabPanel>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item sm={12} md={8} lg={10} className="card1"></Grid>
-        </Grid>
+        </TabContext>
+        {/* <Grid container spacing={2}>
+          <TabContext>
+            <Grid item xs={2}>
+              <Box className="card1">
+                <Item>xs=8</Item>
+              </Box>
+            </Grid>
+            <Grid item xs={10}>
+              <Item>xs=4</Item>
+            </Grid>
+          </TabContext>
+        </Grid> */}
       </Box>
     </Box>
   );
